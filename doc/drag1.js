a1078f1dc1070f7e462dc2aeb9a60e59f370cc35
zhangqiuhong <fallred@sina.com>
2023-05-20 22:41:53 +0800

    feat: fix drag

---
+++ doc/drag1.js
@@ -0,0 +1,19 @@
+import { fromEvent, withLatestFrom, takeUntil, switchMap, throttleTime, debounceTime, from } from 'rxjs';
+
+const draggableElement = document.getElementById('draggable')
+const mouseDown$ = fromEvent(draggableElement, 'mousedown');
+const mouseMove$ = fromEvent(draggableElement, 'mousemove').pipe(throttleTime(30));
+const mouseUp$ = fromEvent(draggableElement, 'mouseup');
+
+mouseDown$.pipe(
+    switchMap(() => mouseMove$.pipe(takeUntil(mouseUp$.pipe(debounceTime(500))))),
+    withLatestFrom(mouseDown$, (moveEvent, downEvent) => {
+        return {
+            left: moveEvent.clientX - downEvent.offsetX,
+            top: moveEvent.clientY - downEvent.offsetY
+        }
+    })
+).subscribe(({ left, top }) => {
+    draggableElement.style.left = left + 'px';
+    draggableElement.style.top = top + 'px';
+}) 
